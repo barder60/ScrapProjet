31ef3c5eb071eba6a168702042b103abf0bae1b5
lucas pothier <kaderate.yamamoto@hotmail.fr>
2019-10-12 11:25:51 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/ScrapProjet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ScrapProjet.dir/main.c.o"
+  "ScrapProjet"
+  "ScrapProjet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ScrapProjet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ScrapProjet.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ScrapProjet.
+# This may be replaced when dependencies are built.
